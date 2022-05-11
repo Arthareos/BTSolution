b615782213466b1764a37c9d3e1dc625fbe9e033
Simone Di Fonzo <difonzosimone@hotmail.com>
2022-05-11 21:20:51 +0300

    Added services

---
+++ BTSolution/Data/UserService.cs
@@ -0,0 +1,58 @@
+﻿//  --------------------------------------------------------------------------------------------
+//  <Copyright>
+//      Copyright © 2022 Simone Di Fonzo. All rights reserved.
+//  </Copyright>
+//  --------------------------------------------------------------------------------------------
+
+using BTSolution.BL.Interfaces;
+using BTSolution.DAL.DTO;
+
+
+namespace BTSolution.Data;
+
+public class UserService
+{
+    #region Members
+
+    private readonly IUserLogic _userLogic;
+
+    #endregion
+
+    #region Constructors
+
+    public UserService(IUserLogic userLogic)
+    {
+        _userLogic = userLogic;
+    }
+
+    #endregion
+
+    #region Methods - Public
+
+    public async void AddUser(UserDTO userDto)
+    {
+        await Task.Run(() => _userLogic.AddUser(userDto));
+    }
+
+    public async void DeleteUser(int userId)
+    {
+        await Task.Run(() => _userLogic.DeleteUser(userId));
+    }
+
+    public async Task<UserDTO> GetUserById(int userId)
+    {
+        return await Task.FromResult(_userLogic.GetUserById(userId));
+    }
+
+    public async Task<IEnumerable<UserDTO>> GetUsers()
+    {
+        return await Task.FromResult(_userLogic.GetUsers());
+    }
+
+    public async void UpdateUser(UserDTO userDto)
+    {
+        await Task.Run(() => _userLogic.UpdateUser(userDto));
+    }
+
+    #endregion
+}
--- BTSolution/Program.cs
+++ BTSolution/Program.cs
@@ -4,20 +4,36 @@
 //  </Copyright>
 //  --------------------------------------------------------------------------------------------
 
+using BTSolution.BL.Classes;
+using BTSolution.BL.Interfaces;
 using BTSolution.DAL.Migrations;
+using BTSolution.DAL.Repository.Classes;
+using BTSolution.DAL.Repository.Interfaces;
 using BTSolution.Data;
 
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add DB context
+string connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<BTSolutionDbContext>(options => options.UseSqlServer(connectionString));
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
-string connectionString = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddDbContext<BTSolutionDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddTransient<IAccessTokenRepository, AccessTokenRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+builder.Services.AddTransient<IAccessTokenLogic, AccessTokenLogic>();
+builder.Services.AddTransient<IUserLogic, UserLogic>();
+
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<AccessTokenService>();
+builder.Services.AddScoped<WeatherForecastService>();
 
 var app = builder.Build();
 
